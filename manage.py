95693970a810b9b62b7474bfafeaf0ae82a771af
blairt001 <blairt371.dev@gmail.com>
2019-01-08 23:35:09 +0300

    [Feature #163047251] Refactor code

--- manage.py
+++ manage.py
@@ -9,11 +9,8 @@ load_dotenv()
 APP_ENV = os.getenv("APP_ENV")
 app = create_app(APP_ENV)
 
-manager = Manager(app)
-
-@manager.command
 def test():
     pytest.main(['-v', '--cov-report', 'term-missing', '--cov=app'])
 
 if __name__ == "__main__":
-    manager.run()
+    app.run()
