99e6f720015234ca70bd04a233c287b6206f7386
blairt001 <blairt371.dev@gmail.com>
2019-01-08 23:01:09 +0300

    [Fix #163033755] Fix whitespace

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,6 @@ from flask import Flask, Blueprint
 from app.api.v1.admin.routes import path_1 as meetups
 from config import app_config
 
-
-
 def create_app(app_environment):
     app = Flask(__name__)
     app.config.from_object(app_config[app_environment])
