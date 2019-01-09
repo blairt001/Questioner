90c0f46feb805bc76d75236a140dc23dd2d7d801
blairt001 <blairt371.dev@gmail.com>
2019-01-09 10:32:42 +0300

    [chore  #163047914] Refactor code

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,13 @@
 Return the app after creating our function
 """
 
-from flask import Flask, Blueprint
+from flask import Flask, jsonify, Blueprint
 from app.api.v1.admin.routes import path_1 as meetups
 from config import app_config
 
-def create_app(config_filename):
+def create_app(app_environment):
     app = Flask(__name__)
-    app.config.from_object(config_filename)
+    app.config.from_object(app_config[app_environment])
     app.register_blueprint(meetups)
 
     return app
-
-if __name__ == "__main__":
-    app = create_app("config")
-    app.run(debug=True)
--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import os
 from flask_script import Manager
 from app import create_app
 import pytest
@@ -5,12 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = create_app("config.BaseConfig")
-runner = Manager(app)
+APP_ENV = os.getenv("APP_ENV")
+app = create_app(APP_ENV)
 
-@runner.command
+manager = Manager(app)
+
+@manager.command
 def test():
     pytest.main(['-v', '--cov-report', 'term-missing', '--cov=app'])
 
 if __name__ == "__main__":
-    runner.run()
+    manager.run()
