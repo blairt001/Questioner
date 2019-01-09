2f7b6929b627203a5ca94ba0832e3d0dbf03573e
blairt001 <blairt371.dev@gmail.com>
2019-01-09 10:10:05 +0300

    [Feature #163047251] Refactor code

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,13 @@ from flask import Flask, Blueprint
 from app.api.v1.admin.routes import path_1 as meetups
 from config import app_config
 
-def create_app(app_environment):
+def create_app(config_filename):
     app = Flask(__name__)
-    app.config.from_object(app_config[app_environment])
+    app.config.from_object(config_filename)
     app.register_blueprint(meetups)
 
     return app
+
+if __name__ == "__main__":
+    app = create_app("config")
+    app.run(debug=True)
