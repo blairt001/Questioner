2fffe1bb8365bf26f1862149a2c595beace20a9e
blairt001 <blairt371.dev@gmail.com>
2019-01-09 11:17:34 +0300

    [Fixure #163047251] modified manage.py

--- manage.py
+++ manage.py
@@ -1,19 +1,12 @@
 import os
 from flask_script import Manager
-from app import create_app
-import pytest
 from dotenv import load_dotenv
 
-load_dotenv()
+from app import create_app
 
 APP_ENV = os.getenv("APP_ENV")
 app = create_app(APP_ENV)
 
-manager = Manager(app)
 
-@manager.command
-def test():
-    pytest.main(['-v', '--cov-report', 'term-missing', '--cov=app'])
-
-if __name__ == "__main__":
-    manager.run()
+if __name__ == '__main__':
+    app.run(debug=True)
