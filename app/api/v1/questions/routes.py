384d506342a12c9f59c3cd51edc74f27b3086110
blairt001 <blairt371.dev@gmail.com>
2019-01-11 23:44:31 +0300

    [Feature #163047604]Implements API endpoint route for user get all questions

--- app/api/v1/questions/routes.py
+++ app/api/v1/questions/routes.py
@@ -74,3 +74,13 @@ def user_comment_on_a_question(question_id):
         my_question['comments'].append(comment)
         return jsonify({"status": 201, "data": my_question}), 201
     return jsonify({'status': 404, 'error':'The question you are looking for is not found'}), 404
+
+@path_1.route("/meetups/<int:meet_id>/questions", methods=['GET'])
+def get_user_get_all_questions_for_a_meetup(meet_id):
+    """
+    User to fetch all questions for a meetup record
+    """
+    questions = QuestionModel.get_all_questions(meet_id)
+    if questions:
+        return jsonify({"status": 200, "data": questions}), 200
+    return jsonify({"status": 404, "data": "We cant find a question for this meetup. No question posted yet"}), 404
