1dfd6b5ceae6dc3a819d849d5f23a6f8b34f5711
blairt001 <blairt371.dev@gmail.com>
2019-01-12 22:05:54 +0300

    [Feature #163047522] Implements user login endpoint

--- app/api/v1/users/routes.py
+++ app/api/v1/users/routes.py
@@ -35,3 +35,23 @@ def user_sign_up():
     #call the save_user method from the models
     user.save_user()
     return jsonify({"status":201, "data":"User Registered Successfully!"}), 201
+
+#login route
+@path_1.route("/auth/login", methods=['POST'])
+def user_login():
+    try:
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+    except KeyError:
+        abort(make_response(jsonify({'status': 400,
+                                     ' error': "Check your json keys and try again. Make sure it is username and password"}), 400))
+
+    verify_if_admin(username)  #rem we had set isAdmin to false
+
+    user = UserModel.query_users(username, password)
+    if not user:
+        return jsonify({"status": 400, "data":"Please Register First to Login"}), 400
+
+    token = jwt.encode({"username":username}, KEY, algorithm='HS256')
+    return jsonify({"status": 200, "token":token.decode('UTF-8'), "message": "You have Logged in Successfully}), 200
